75c670c6b03c5594d906b44c4fd54266edfe0f39
adhiendraadheera-oss <adhiendraadheera@gmail.com>
2026-09-25 11:32:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api_key = st.sidebar.text_input("Enter Gemini API Key:", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash")
 
     # Inputs
     st.header("📊 Input Your Financial Details")
